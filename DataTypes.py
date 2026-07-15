7a1e8f2a3e947372095be3b3476f1ea57841ecad
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-15 08:42:07 +0530

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,21 @@
+a = 10
+b = -110
+print(type(a))
+print(type(b))
+
+#Float
+a=10.5
+print(type(a))
+
+#complex
+a=3+3j
+print(type(a))
+
+#Boolean
+a=True
+b=False
+print(type(a))
+
+a=10
+b=15
+print(a==b)
