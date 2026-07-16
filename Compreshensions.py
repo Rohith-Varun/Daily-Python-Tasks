f02fe059cbcc0acf640c28d73dbd91fc46ea58d1
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-16 08:45:41 +0530

    Add files via upload

---
+++ Compreshensions.py
@@ -0,0 +1,18 @@
+'''l=[i for i in range(1,11)]
+print(l)
+
+l = [1,2,3,4,5,6,7,8,9,10]
+a = [i for i in l if i%2==0]
+print(a)
+
+#l = [1,2,3,4,5,6,7,8,9,10]
+a= ["Even" if i%2==0 else "Odd" for i in range(1,5)]
+print(a)
+
+#l = [1,2,3,4,5,6,7,8,9,10]
+a= ["Even" if i%2==0 else "Odd" for i in range(1,5)]
+print(a)'''
+
+a=["p", "y", "t", "h", "o", "n"]
+for index, value in enumerate(a):
+    print(index, value)
