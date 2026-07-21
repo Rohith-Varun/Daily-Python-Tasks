746a87216a0b24d1aa73412b0db761891433c556
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-21 11:54:13 +0530

    Add files via upload

---
+++ Loopings.py
@@ -0,0 +1,27 @@
+'''a = int(input(" Table : "))
+for i in range (1,11,1):
+    print(a, 'x',  i, " = " ,(a*i))'''
+
+#a = int(input("Enter the range : "))
+#res=[]
+'''for i in range(1,100,1):
+    print(i%2==0,+i)'''
+    
+'''for i in range(1,101,1):
+    if i%2==0:
+        print(i)'''
+'''a=int(input("Enter Value : "))
+if a%a==0 and a%1==a:
+    print("Prime")
+else:
+    print("Not Prime")'''
+    
+'''n=[[1 ,2, 3], [4, 5, 6], [7, 8, 9]]
+for i in range(len(n)):
+    for j in range(len(n[0])):
+        print(n[i][j])    
+print(n)'''
+
+for i in range(0,11):
+    print("😝💀🐚")
+
