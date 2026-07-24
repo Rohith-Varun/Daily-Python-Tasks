dc7fa92f80df9a87dc74f317501a1f16609531d0
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-24 11:12:06 +0530

    Add files via upload

---
+++ Practice_Prblms.py
@@ -0,0 +1,24 @@
+#Add 2 nums and print res
+'''a = int(input(" Enter A Value : "))
+b = int(input(" Enter B Value : "))
+c = a+b
+d = a%b
+print(c)
+print(d)'''
+
+#checking the num odd or eve
+'''a = int(input("Enter A value: "))
+if a%2==0:
+    print("Even")
+else:
+    print("Odd")'''
+    
+#swapping 2 nums without 3rd var
+a = int(input("Enter A value: "))
+b = int(input("Enter B value: "))
+print(a)
+print(b)
+a = b
+b = a
+print(a)
+print(b)
