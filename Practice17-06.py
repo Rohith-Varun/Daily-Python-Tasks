26da4734ba017229778df1c07b73dd0578acd9ed
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-25 06:59:32 +0530

    Add files via upload

---
+++ Practice17-06.py
@@ -0,0 +1,85 @@
+''''print("Varun")
+print(21)
+print("Vijayawada")
+print("---")
+
+print("--------------------------------------------------------------------------------------------")
+
+print("SIR. C. R. REDDY COLLEGE OF ENGINNERING")
+print("CSE")
+print("2022 to 2026")
+print()
+
+print("--------------------------------------------------------------------------------------------")
+
+print("Varun")
+print("SIR. C. R. REDDY COLLEGE OF ENGINNERING")
+print("CSE")
+print("---")
+
+print("--------------------------------------------------------------------------------------------")
+
+print("09:00 AM - Python")
+print("11:00 AM - Appitude")
+print("02:00 AM - SoftSkills")
+print("04:00 AM - Practice Session")
+
+print("--------------------------------------------------------------------------------------------")'''
+
+#using Variable
+print("USED VARIABLES:")
+a="Varun"
+print(a)
+
+b=21
+print(b)
+
+c="Vijayawada"
+print(c)
+
+d="--"
+print(d)
+
+print("--------------------------------------------------------------------------------------------")
+
+a="SIR. C. R. REDDY COLLEGE OF ENGINEERING"
+print(a)
+
+b="CSE"
+print(b)
+
+c="2022 - 2026"
+print(c)
+
+d="--"
+print(d)
+
+print("--------------------------------------------------------------------------------------------")
+
+a="varun"
+print(a)
+
+b="SIR. C. R. REDDY COLLEGE OF ENGINNERING"
+print(b)
+
+c="cse"
+print(c)
+
+d="--"
+print(d)
+
+print("--------------------------------------------------------------------------------------------")
+
+a="09:00 AM - Python"
+print(a)
+
+b="11:00 AM - Appitude"
+print(b)
+
+c="02:00 AM - SoftSkills"
+print(c)
+
+d="04:00 AM - Practice Session"
+print(d)
+
+print("--------------------------------------------------------------------------------------------")
