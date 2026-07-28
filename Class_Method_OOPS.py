c757e4a0786a56ded016e59adcaab23c99783ff2
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-28 11:02:20 +0530

    Add files via upload

---
+++ Class_Method_OOPS.py
@@ -0,0 +1,8 @@
+class Student:
+    collegename = "CRRCOE"
+    @classmethod #@classmethod - this is a decorator which is used to define the class method if not defined then only it becomes the class method and the class variable can be accessed outside the class
+    def GetCollegeInfo(cls): #(cls) - this must be mentioned for the class method then only then only the class variable can be accessed outside the class
+        print("My clg name is :", cls.collegename) #classname.methodname is used to access the outside the class
+    
+print(Student.collegename)
+Student.GetCollegeInfo()
