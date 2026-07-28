13d0b30ac212154dc64ed3d8660e58a31b69f4b1
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-28 11:01:51 +0530

    Add files via upload

---
+++ ClassVar_OOPS.py
@@ -0,0 +1,14 @@
+class Student:
+    collegename = "CRRCOE"
+    
+    def __init__(self):
+        self.name = "Varun"
+        self.age = 21
+        self.dept = "CSE"
+s1 = Student()
+print(s1.collegename)
+print(s1.name)
+print(Student.collegename)
+
+s2 = Student()
+print(s2.collegename)
