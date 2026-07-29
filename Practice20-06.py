3287b052202c062ab1da7ea72c63abe97d2001e5
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-29 11:22:05 +0530

    Fix indentation and formatting in eligibility checks

--- Practice20-06.py
+++ Practice20-06.py
@@ -1,8 +1,8 @@
-'''CS=int(input("Enter Credit Score : "))
+CS=int(input("Enter Credit Score : "))
 IC=int(input("Enter Income : "))
 LA=int(input("Enter Liabilities : "))
 
-if CS>=750 and IC>=50000 and LA<=20000:
+'''if CS>=750 and IC>=50000 and LA<=20000:
     print("Eligible")
 elif CS<=749 and CS>=650 and IC>=50000 and LA<=20000:
     print("Conditions Apply")
