0c61d87e917ff841d51e34bd972617cb0f810d11
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-29 11:17:32 +0530

    Add files via upload

---
+++ Practice20-06.py
@@ -0,0 +1,12 @@
+'''CS=int(input("Enter Credit Score : "))
+IC=int(input("Enter Income : "))
+LA=int(input("Enter Liabilities : "))
+
+if CS>=750 and IC>=50000 and LA<=20000:
+    print("Eligible")
+elif CS<=749 and CS>=650 and IC>=50000 and LA<=20000:
+    print("Conditions Apply")
+else:
+    print("Not Eligible")'''
+    
+    
