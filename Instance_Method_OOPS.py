b1c8096ce7c5c273c10da24d8c5e6c6aeb1c7bd6
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-30 11:19:26 +0530

    Refactor Student class formatting and structure

--- Instance_Method_OOPS.py
+++ Instance_Method_OOPS.py
@@ -1,11 +1,9 @@
 class Student:
     collegename = "CRRCOE"
-    
     def __init__(self):
         self.name = "Varun"
         self.age = 21
-        self.dept = "CSE"
-        
+        self.dept = "CSE"  
     def display(self):
         print("Name:", self.name)
         print("Age:", self.age)
