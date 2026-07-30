cdd629d93922f7289a74a59ac16661c06e8454ef
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-30 11:18:59 +0530

    Add files via upload

---
+++ Instance_Method_OOPS.py
@@ -0,0 +1,15 @@
+class Student:
+    collegename = "CRRCOE"
+    
+    def __init__(self):
+        self.name = "Varun"
+        self.age = 21
+        self.dept = "CSE"
+        
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Department:", self.dept)
+        print("College Name:", Student.collegename)
+s1 = Student()
+s1.display()
