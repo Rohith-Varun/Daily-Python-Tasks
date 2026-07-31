e4e4a041e165e345cf81014f074160ec9e9508cc
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-31 09:15:53 +0530

    Refactor list comprehensions in Compreshensions.py
    Refactored list comprehensions and print statements for clarity.

--- Compreshensions.py
+++ Compreshensions.py
@@ -6,11 +6,9 @@ print(a)
 l = [1,2,3,4,5,6,7,8,9,10]
 a= ["Even" if i%2==0 else "Odd" for i in range(1,5)]
 print(a)
-
 l = [1,2,3,4,5,6,7,8,9,10]
 a= ["Even" if i%2==0 else "Odd" for i in range(1,5)]
 print(a)
-
 a=["p", "y", "t", "h", "o", "n"]
 for index, value in enumerate(a):
     print(index, value)
