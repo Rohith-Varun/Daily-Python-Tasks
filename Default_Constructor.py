66efc8642a7bd389b7f16278a462f349cb10daa4
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-07-31 09:14:38 +0530

    Fix display call for second student instance
    Added display call for s2 instance.

--- Default_Constructor.py
+++ Default_Constructor.py
@@ -1,16 +1,13 @@
 class Student:
-    collegename = "CRRCOE"
-    
+    collegename = "CRRCOE" 
     def __init__ (self,name,age,marks):
         self.name = name
         self.age = age
         self.marks = marks
-    
     def display(self):
         print("My name is : ", self.name)
         print("My age is : ", self.age)
-        print("My marks is : ", self.marks)
-        
+        print("My marks is : ", self.marks)      
 s1 = Student("Varun", 22, 90)
 s2 = Student("Rohith", 21, 80)
 s1.display()
