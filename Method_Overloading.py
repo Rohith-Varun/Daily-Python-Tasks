8573f8da1cef72c3299530b6c9ebb9c9620be3ba
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-03 11:24:34 +0530

    Refactor method overloading examples in Method_Overloading.py

--- Method_Overloading.py
+++ Method_Overloading.py
@@ -2,33 +2,28 @@
 
 class Test:
     def m1(self):
-        print("No arguments menthos")
-        
+        print("No arguments menthos")        
     def m1(self,a):
-        print("method with one argument")
-        
+        print("method with one argument")     
     def m1(self,a,b):
         print("method with two args")
-
 t = Test()
 t.m1("k", "l")
 #default args,"args
 
 #method overloading using variable length arguments 
-'''class Test():
+class Test():
     def add(self, *l):
-        print(sum(l))
-        
+        print(sum(l))        
 t = Test()
 t.add(10,20)
 t.add(10,20,30)
-t.add(10,20,30,40)'''
+t.add(10,20,30,40)
 
 #by using default length arguments 
 class Test():
     def add(self, a = 0, b = 0, c = 0):
-        print(a+b+c)
- 
+        print(a+b+c) 
 t = Test()
 t.add(10)
 t.add(10,20)
