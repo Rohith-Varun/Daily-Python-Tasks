1e4853757540416c1194705afe0eb5ae1bc97c69
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-05 11:27:01 +0530

    Update Multilevel_Inheritance.py

--- Multilevel_Inheritance.py
+++ Multilevel_Inheritance.py
@@ -1,29 +1,22 @@
 class Animal:#Grand parent class
     def Eat(self):
         print("Animal Eats")
-
 class Dog(Animal): #Dog -> inherites the property from Animal of eat PARENT CLASS
     def Bark(self):
-        print("Dogs Barks")
-        
+        print("Dogs Barks")      
 class Babydog(Dog):#Child class Inherits fronm the parent class of DOG
     def Cry(self):
-        print("Baby Dog Cries")
-        
+        print("Baby Dog Cries")       
 a = Babydog()
 a.Cry()
 a.Bark()
 a.Eat()  
-
-
 class Intern:
     def Fresher(self):
         print("Learning")
-
 class JuniorDev(Intern):
     def Juniordeveloper(self):
-        print("Practicing")
-        
+        print("Practicing")        
 class SeniorDev(JuniorDev):
     def Seniordeveloper(self):
         print("Teaching")
