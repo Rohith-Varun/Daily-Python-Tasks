19ddb206ae76e3b6d68dc13f546e7d0619e00d8d
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-08 10:37:14 +0530

    Format code for consistency in Instance_Method_OOPS.py

--- Instance_Method_OOPS.py
+++ Instance_Method_OOPS.py
@@ -3,8 +3,7 @@ class Student:
     def __init__(self):
         self.name = "Varun"
         self.age = 21
-        self.dept = "CSE"
-        
+        self.dept = "CSE"      
     def display(self):
         print("Name:", self.name)
         print("Age:", self.age)
