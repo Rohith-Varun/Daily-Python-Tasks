bb789b60278604945e81a73594f2380fd8c483fd
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-10 11:15:02 +0530

    Add files via upload

--- Method_Operator_Overloading.py
+++ Method_Operator_Overloading.py
@@ -1,15 +1,19 @@
 class Point:
     def __init__(self, x):
         self.x = x
+    
     def __str__(self):
         return str(self.x)
     def __add__ (self, other):
         #return self.x + other.x #only used for the 2 operators 
         return Point(self.x + other.x) #this is used for multiple operators(more than 2)
+    
 p1 = Point(5)
 print(p1)
+
 p2 = Point(4)
 print(p2)
+
 p3 = Point(6)
 print(p3)
 print(p1+p2)
