51f473ee0b06bf8e7e879113f1413862b62ada59
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-13 07:34:51 +0530

    Fix formatting issues in OOPS_Concepts.py

--- OOPS_Concepts.py
+++ OOPS_Concepts.py
@@ -1,13 +1,10 @@
 #syntax for creating a class
 class MyClass:
     x = 5
-
 #syntax for creating an object of a class
 p1 = MyClass()
-
 #syntax for accessing the class attributes
 print(p1.x)
-
 #syntax for accessing the class using the object
 class Person:
     def __init__(self, name, age):
