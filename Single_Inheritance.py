318f4ad55e238b967b56422f210cc74ebf834ae1
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-14 08:04:51 +0530

    Add files via upload

---
+++ Single_Inheritance.py
@@ -0,0 +1,15 @@
+class Animal():
+    def Sound(self):
+        print("Animals Makes Sounds")
+    
+class Dog(Animal):
+    def Bark(self):
+        print("Dog is Barking")
+
+#a = Animal()
+#a.Sound()
+#a.Bark
+
+d = Dog()
+d.Bark()
+d.Sound()
