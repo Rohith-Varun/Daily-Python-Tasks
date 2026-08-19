9e293667f75fac84bda5a968acb97c8232c9205a
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-19 11:16:23 +0530

    Add marks input and average calculation

---
+++ practice18-06 3.py
@@ -0,0 +1,22 @@
+a=int(input("Enter Your marks of Sub 1 : "))
+print("marks in sub1: ", +a)
+
+b=int(input("Enter Your marks of Sub 2 : "))
+print("marks in sub2: ", +b)
+
+c=int(input("Enter Your marks of Sub 3 : "))
+print("marks in sub3: ", +c)
+
+d=int(input("Enter Your marks of Sub 4 : "))
+print("marks in sub4: ", +d)
+
+e=int(input("Enter Your marks of Sub 5 : "))
+print("marks in sub5: ", +e)
+
+f=(a+b+c+d+e)
+print("Total", +f)
+
+g=(f/5)
+print("Avg", +g)
+
+print("------------------------------------------------------------------------------------------------------------------")
