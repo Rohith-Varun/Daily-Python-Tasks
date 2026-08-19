b8090388dd78b479101ad8c399b1ab4347f2c893
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-19 11:12:59 +0530

    Update Practice18-06.py

--- Practice18-06.py
+++ Practice18-06.py
@@ -62,24 +62,6 @@ print("Avg", +g)
 
 print("------------------------------------------------------------------------------------------------------------------")'''
 
-'''a=int(input("enter DOB in Year : "))
-print("Your Age :", +2026-a)'''
-
-'''a=int(input("enter the length : "))
-b=int(input("enter the bredth : "))
-c= a*b
-print("area: ",+c)'''
-
-'''a=int(input("Enter Radius: "))
-b=(3.14*a*a)
-print(b)'''
-
-'''p=int(input("Enter P: "))
-t=int(input("Enter T: "))
-r=int(input("Enter R: "))
-a=((p*t*r)/100)
-print(a)'''
-
 a=20
 b=10
 print(a+b)
