015f518d2026ab076857864d10012b1603cded5f
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-19 11:12:07 +0530

    Add files via upload

---
+++ Practice18-06.py
@@ -0,0 +1,88 @@
+'''full_name="ROHITH VARUN PONNADA"
+print(full_name)
+print()
+
+headline="Python FullStack Trainee at Codegnan | Learning Python, Flask, React & Web Development"
+print(headline)
+print()
+
+email="rohithvarunponnada@gmail.com"
+print(email)
+print()
+
+about="Python Full Stack Trainee at Codegnan and B.Tech CSE student. Skilled in Python, Java, C, HTML, CSS, JavaScript, and SQL, with a keen interest in software development. Eager to apply my technical skills to real-world projects and grow as a Full Stack Developer."
+print(about)
+print()
+
+exp="Fresher"
+print(exp)
+print()
+
+compamy="Codegnan"
+print(compamy)
+print()
+
+role="Full Stack Trainee at Codegnan"
+print(role)
+print()
+
+year="Jun 2026 - Present"
+print(year)'''
+
+'''print("------------------------------------------------------------------------------------------------------------------")
+
+a= "vodka - Magic Moments"
+b= "wiskey - Signature"
+c= "rum -  Old Monk"
+d= "Beer - Kingfisher"'''
+
+'''print("------------------------------------------------------------------------------------------------------------------")
+
+
+a=int(input("Enter Your marks of Sub 1 : "))
+print("marks in sub1: ", +a)
+
+b=int(input("Enter Your marks of Sub 2 : "))
+print("marks in sub2: ", +b)
+
+c=int(input("Enter Your marks of Sub 3 : "))
+print("marks in sub3: ", +c)
+
+d=int(input("Enter Your marks of Sub 4 : "))
+print("marks in sub4: ", +d)
+
+e=int(input("Enter Your marks of Sub 5 : "))
+print("marks in sub5: ", +e)
+
+f=(a+b+c+d+e)
+print("Total", +f)
+
+g=(f/5)
+print("Avg", +g)
+
+print("------------------------------------------------------------------------------------------------------------------")'''
+
+'''a=int(input("enter DOB in Year : "))
+print("Your Age :", +2026-a)'''
+
+'''a=int(input("enter the length : "))
+b=int(input("enter the bredth : "))
+c= a*b
+print("area: ",+c)'''
+
+'''a=int(input("Enter Radius: "))
+b=(3.14*a*a)
+print(b)'''
+
+'''p=int(input("Enter P: "))
+t=int(input("Enter T: "))
+r=int(input("Enter R: "))
+a=((p*t*r)/100)
+print(a)'''
+
+a=20
+b=10
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
