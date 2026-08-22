a53eb75ee5e73d4d8930e3df4968b4b7dab293ec
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-22 07:40:52 +0530

    Refactor numpy array examples in npy_cls1.py

--- npy_cls1.py
+++ npy_cls1.py
@@ -9,10 +9,10 @@ print(numbers)
 print(type(l))
 print(type(numbers))
 
-'''import numpy as np
+import numpy as np
 num = np.array([[1,2,3], [3,4,5]]) #for creating the 2D array
-print(num)'''
+print(num)
 
-'''import numpy as np 
+import numpy as np 
 num = np.array([[[1,2],[2,3]],[[4,5],[5,6]]]) #3D Array
-print(num)'''
+print(num)
