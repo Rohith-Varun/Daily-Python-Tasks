9389af31693565bd1351db2143928988a403b0b3
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-22 07:39:55 +0530

    Add files via upload

---
+++ npy_cls1.py
@@ -0,0 +1,18 @@
+'''import numpy as np
+numbers = np.array([1,2,3])
+print(numbers)'''
+
+'''import numpy as np
+l = [1,2,3]
+numbers = np.array(l)
+print(numbers)
+print(type(l))
+print(type(numbers))'''
+
+'''import numpy as np
+num = np.array([[1,2,3], [3,4,5]]) #for creating the 2D array
+print(num)'''
+
+'''import numpy as np 
+num = np.array([[[1,2],[2,3]],[[4,5],[5,6]]]) #3D Array
+print(num)'''
