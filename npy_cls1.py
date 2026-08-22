9890f1ee280274bc8db2bec39be674b3234f4e7a
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-22 07:41:26 +0530

    Reorganize and clean up numpy array examples

--- npy_cls1.py
+++ npy_cls1.py
@@ -13,6 +13,8 @@ import numpy as np
 num = np.array([[1,2,3], [3,4,5]]) #for creating the 2D array
 print(num)
 
+print("---------------------------------------------------")
+
 import numpy as np 
 num = np.array([[[1,2],[2,3]],[[4,5],[5,6]]]) #3D Array
 print(num)
