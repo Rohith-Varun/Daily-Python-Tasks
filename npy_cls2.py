ad2d5341c290bcd7729d2a33c4446f3f625a0eda
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-23 10:35:41 +0530

    Add files via upload

---
+++ npy_cls2.py
@@ -0,0 +1,23 @@
+'''import numpy 
+zeros = numpy.zeros((2,3)) #all the elements becomes zeros
+print(zeros)
+ones = numpy.ones((4,4)) #all the elements becomes ones
+print(ones)
+eye = numpy.eye(3) #digonal elementd becomes 1
+print(eye)'''
+
+'''import numpy as np
+rand_int = np.random.randint(1,100,(3,3))
+print(rand_int)
+'''
+'''
+import numpy
+r = numpy.random.rand(3,3) #used to generate the values between 0 t0 10
+print(r)
+print(range)'''
+
+'''import numpy
+numpy.random.seed(10)
+r = numpy.random.choice([10,20,30,40,50,60,70,80], 5)
+print(r)
+'''
