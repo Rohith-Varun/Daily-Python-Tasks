3fe54c97eae4d0d589ab296a99f943f46ca1062d
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-23 10:36:25 +0530

    Refactor numpy usage and print statements
    Reorganized numpy imports and print statements for clarity.

--- npy_cls2.py
+++ npy_cls2.py
@@ -1,16 +1,15 @@
-'''import numpy 
+import numpy 
 zeros = numpy.zeros((2,3)) #all the elements becomes zeros
 print(zeros)
 ones = numpy.ones((4,4)) #all the elements becomes ones
 print(ones)
 eye = numpy.eye(3) #digonal elementd becomes 1
-print(eye)'''
+print(eye)
 
-'''import numpy as np
+import numpy as np
 rand_int = np.random.randint(1,100,(3,3))
 print(rand_int)
-'''
-'''
+
 import numpy
 r = numpy.random.rand(3,3) #used to generate the values between 0 t0 10
 print(r)
