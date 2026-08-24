af9c30945e99c62d78c7ad500194da1e9023fc99
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-24 11:19:57 +0530

    Refactor npy_cls3.py by removing commented code
    Removed commented-out code and redundant imports. Cleaned up the script for clarity.

--- npy_cls3.py
+++ npy_cls3.py
@@ -18,11 +18,11 @@ print(sum(data))
 print(min(data))
 print(max(data))
 print(data.mean())
-'''
+
 import numpy as np
 data = np.array([1,2,3,4,5,5,6])
 print(data.mean())
-print(np.mean(data))'''
+print(np.mean(data))
 
 
 
