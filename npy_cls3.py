df704af0934cd385b28d9e94ac622672abc2218a
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-24 11:18:13 +0530

    Add files via upload

---
+++ npy_cls3.py
@@ -0,0 +1,29 @@
+'''import numpy as np
+data = np.arange(1,10,2) #start, stop, step
+print(data)'''
+
+'''import numpy as np
+data = np.linspace(1,25,5) #linerspace sequence of numbers the numbers start and end muct involved and also the step, stop, num
+print(data)'''
+
+#how to perform element wise operations 
+'''import numpy as np
+data = np.array([1,2,3,4,5,6]) #the each element in the array will be added with 10 
+print(data)
+print(data + 10) #the data in the array will be added with the value of 10 1 -> 10.'''
+
+'''import numpy as np
+data = np.array([1,2,3,4,5,6]) 
+print(sum(data))
+print(min(data))
+print(max(data))
+print(data.mean())
+'''
+'''
+import numpy as np
+data = np.array([1,2,3,4,5,5,6])
+print(data.mean())
+print(np.mean(data))'''
+
+
+
