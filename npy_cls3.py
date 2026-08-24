30804042bf5297912ef5699b295f79fda76b365b
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-24 11:19:36 +0530

    Remove duplicate imports and clean up code

--- npy_cls3.py
+++ npy_cls3.py
@@ -12,13 +12,12 @@ data = np.array([1,2,3,4,5,6]) #the each element in the array will be added with
 print(data)
 print(data + 10) #the data in the array will be added with the value of 10 1 -> 10.
 
-'''import numpy as np
+import numpy as np
 data = np.array([1,2,3,4,5,6]) 
 print(sum(data))
 print(min(data))
 print(max(data))
 print(data.mean())
-'''
 '''
 import numpy as np
 data = np.array([1,2,3,4,5,5,6])
