47ece0cf08d797b1193e4b6b452cff908c10ceb2
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-25 08:26:33 +0530

    Reorganize and uncomment numpy array examples

--- npy_cls4.py
+++ npy_cls4.py
@@ -12,12 +12,12 @@ print(data[0])
 print(data[3])
 print(data[5])
 
-'''import numpy as np
+import numpy as np
 data = np.array([1,2,3,4,5,6])
 print(data[2])
 print(data[-1])
 print(data[1 : 4])
-print(data [ :3])'''
+print(data [ :3])
 
 '''import numpy as np
 data = np.array([[1,2,3], [4,5,6]])
