e2f176a7f91740a16ee031f96869fe8c3c48b50f
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-25 08:24:53 +0530

    Add files via upload

---
+++ npy_cls4.py
@@ -0,0 +1,33 @@
+'''import numpy as np
+data = np.array([[1,2,3],[5,6,7]])
+print(data)
+print(data.shape)
+print(data.reshape(3,2))
+print(data.shape)'''
+
+'''import numpy as np
+data = np.array([1,2,3,4,5])
+print(data)
+print(data[0])
+print(data[3])
+print(data[5])'''
+
+'''import numpy as np
+data = np.array([1,2,3,4,5,6])
+print(data[2])
+print(data[-1])
+print(data[1 : 4])
+print(data [ :3])'''
+
+'''import numpy as np
+data = np.array([[1,2,3], [4,5,6]])
+print(data[0][2])
+print(data[1][1])'''
+
+'''import numpy as np
+data = np.array([1,2,3,4,5])
+print(data)
+data[2] = 99
+print(data)
+newdata = data.view()
+print(newdata)'''
