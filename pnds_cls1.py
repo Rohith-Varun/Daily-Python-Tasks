6d71c147692e2f29729ce4afbb840676e5658143
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-26 11:08:01 +0530

    Add files via upload

---
+++ pnds_cls1.py
@@ -0,0 +1,20 @@
+'''import pandas as pd
+data = pd.Series([10,20,30,40], index = ["Arjun", "Bheema", "Dhrama", "Krishna"])
+print(data)
+
+df = pd.DataFrame({
+    "product" : ["mobile", "laptop", "cart", "bike", "car"],
+    "price" : [100, 200, 400, 500, 600],
+    "quantity" : [3, 5, 6, 1, 6]
+})
+print(df)
+print(df.shape)
+print(df.columns)
+print(df.head) #returns the top 5 rows
+print(df.head(2)) #returns thefirst 2 top no.of rows 
+print(df.tail()) #returns the delfault last 5 rows
+print(df.tail(2)) #returns the last 2 rows
+print("-----------------------")
+print(df.info()) #to get the information of the data 
+print(df.describe())'''
+
