a0b1994bfa43051d69653efcdb15155e6e9e5368
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-28 07:29:46 +0530

    Add files via upload

---
+++ pnds_cls2.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df = pd.DataFrame({
+    "product" : ["mobile", "laptop", "cart", "bike", "car"],
+    "price" : [100, None, 400, 500, 600],
+    "quantity" : [3, 5, 6, 1, None]
+})
+print(df)
+print(df.dropna()) #removes the null elements
+print(df.describe())
