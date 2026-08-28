3b255a95ba2fba186e5ae96a74d43dd6dcdc3989
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-28 07:30:27 +0530

    Update pnds_cls2.py

--- pnds_cls2.py
+++ pnds_cls2.py
@@ -1,8 +1,10 @@
+# pandas files 2
 import pandas as pd
 df = pd.DataFrame({
     "product" : ["mobile", "laptop", "cart", "bike", "car"],
     "price" : [100, None, 400, 500, 600],
     "quantity" : [3, 5, 6, 1, None]
+    
 })
 print(df)
 print(df.dropna()) #removes the null elements
