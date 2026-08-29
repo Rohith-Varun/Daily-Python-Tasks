19b450c5c908783ec2fd8ccf4a0dd6c3118fde94
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-08-29 10:45:44 +0530

    Reformat graph plotting code in matplLib_class1.py

--- matplLib_class1.py
+++ matplLib_class1.py
@@ -1,9 +1,7 @@
 #Graphs
-
 import matplotlib.pyplot as p
 years = [2020, 2021, 2022, 2023, 2024, 2025, 2026]
 admissions = [300, 600, 400, 600, 800, 200, 1500]
-
 p.plot(years, admissions)
 p.xlabel("years")
 p.ylabel("admissions")
