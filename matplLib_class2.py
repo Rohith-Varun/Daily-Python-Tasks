d48ce35147210a11701ee030886842d3375e3c03
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-09-02 07:11:21 +0530

    Add files via upload

---
+++ matplLib_class2.py
@@ -0,0 +1,12 @@
+#Bar Graphs
+
+import matplotlib.pyplot as p
+years = [2020, 2021, 2022, 2023, 2024, 2025, 2026]
+admissions = [300, 600, 400, 600, 800, 200, 1500]
+
+p.bar(years, admissions)
+p.xlabel("years")
+p.ylabel("admissions")
+p.title("CODEGNAN YEAR WISE ADMISSIONS")
+p.grid(True)
+p.show()
