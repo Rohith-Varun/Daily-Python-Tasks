4268dd8ab12f03bb6885a92841e551ac19a9f0c6
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-09-03 07:58:25 +0530

    Reorganize pie chart code in matplLib_class3.py

--- matplLib_class3.py
+++ matplLib_class3.py
@@ -1,9 +1,7 @@
 #Pie Charts
-
 import matplotlib.pyplot as p
 fruits = ["Mango", "Apple", "Banana", "Papayya", "Orange"]
 quantity = [20, 60, 40, 4, 10]
-
 p.pie(quantity, labels = fruits)
 #p.pie(quantity, labels = fruits, autopct="%1.1f%%")#for percentages
 p.show()
