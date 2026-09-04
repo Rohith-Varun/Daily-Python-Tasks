652ebe74e0b8ea4554e364a1ee96866a7c61d000
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-09-04 07:35:14 +0530

    Add files via upload

---
+++ matplLib_class4.py
@@ -0,0 +1,6 @@
+#Histogram charts : ages  Histogram charts -> these are mainly used to represent the data of certain range 
+
+import matplotlib.pyplot as p
+ages = [10,15,18,12,14,15,13,20,20]
+p.hist(ages)
+p.show()
