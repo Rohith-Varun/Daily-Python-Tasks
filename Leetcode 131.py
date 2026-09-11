664d22f1e3165cebc967f1ed6370e1480bb9635f
Rohith Varun Ponnada <rohithvarunponnada@gmail.com>
2026-09-11 07:13:40 +0530

    Implement palindrome partitioning solution

---
+++ Leetcode 131.py
@@ -0,0 +1,23 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        n = len(s)
+        f = [[True] * n for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
+                f[i][j] = s[i] == s[j] and f[i + 1][j - 1]
+        
+        ans = []
+        t = []
+        
+        def dfs(i: int):
+            if i == n:
+                ans.append(t[:])
+                return
+            for j in range(i, n):
+                if f[i][j]:
+                    t.append(s[i:j + 1])
+                    dfs(j + 1)
+                    t.pop()
+                    
+        dfs(0)
+        return ans
